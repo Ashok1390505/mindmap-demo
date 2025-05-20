d1bd4bbcdf53e02d2b6619c3e68cab796add11d6
Ashok1390505 <mandarapuashok39@gmail.com>
2025-05-20 13:23:27 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Mindmap demo latest version !";
+        return "Hello, Mindmap mindmap new version !";
     }
 }
